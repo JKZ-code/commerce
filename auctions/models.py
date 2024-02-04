4725cd6d6ade4b0ec65b643a3c341eb8d0226d7d
Sercifer <brucezhu712@gmail.com>
2024-02-03 20:35:28 -0800

    Updated

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,40 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Type(models.Model):
+    typeName = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.typeName
+
+
+class Bid(models.Model):
+    bid = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userBid")
+
+  
+class Listing(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    imageUrl = models.CharField(max_length=1000)
+    price = models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="bidPrice")
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, blank=True, null=True, related_name="type")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="listingWatchlist")
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userComment")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True, related_name="listingComment")
+    message = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.author} comment on {self.listing}"
+
+
--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,154 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Type, Listing, Comment, Bid
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    activeListings = Listing.objects.filter(isActive=True)
+    allTypes = Type.objects.all()
+    return render(request, "auctions/index.html", {
+        "listings": activeListings,
+        "types": allTypes
+    })
+
+def listing(request, id):
+    listingData = Listing.objects.get(pk=id)
+    isListinginWatchinglist = request.user in listingData.watchlist.all()
+    allComments = Comment.objects.filter(listing=listingData)
+    isOwner = request.user.username == listingData.owner.username
+    return render(request, "auctions/listing.html", {
+        "listing":listingData,
+        "isListinginWatchinglist":isListinginWatchinglist,
+        "allComments":allComments,
+        "isOwner": isOwner
+    })
+
+def closeAuction(request, id):
+    listingData = Listing.objects.get(pk=id)
+    listingData.isActive = False
+    listingData.save()
+    isOwner = request.user.username == listingData.owner.username
+    isListinginWatchinglist = request.user in listingData.watchlist.all()
+    allComments = Comment.objects.filter(listing=listingData)
+
+    return render(request, "auctions/listing.html", {
+        "listing":listingData,
+        "isListinginWatchinglist":isListinginWatchinglist,
+        "allComments":allComments,
+        "isOwner": isOwner,
+        "update": True,
+        "message": "Your auction is closed."
+    })
+
+
+def addComment(request, id):
+    currentUser = request.user
+    listingData = Listing.objects.get(pk=id)
+    message = request.POST["newComment"]
+    newComment = Comment(
+        author=currentUser,
+        listing=listingData,
+        message=message
+    )
+    newComment.save()
+    return HttpResponseRedirect(reverse("listing", args=(id,)))
+
+
+def addBid(request, id):
+    newBid = request.POST["newBid"]
+    listingData = Listing.objects.get(pk=id)
+    isListinginWatchinglist = request.user in listingData.watchlist.all()
+    allComments = Comment.objects.filter(listing=listingData)
+    isOwner = request.user.username == listingData.owner.username
+    if int(newBid) > listingData.price.bid:
+        updateBid = Bid(user=request.user, bid=int(newBid))
+        updateBid.save()
+        listingData.price = updateBid
+        listingData.save()
+        return render(request, "auctions/listing.html", {
+            "listing": listingData,
+            "message": "Successfully Bid!",
+            "update" : True,
+            "isListinginWatchinglist":isListinginWatchinglist,
+            "allComments":allComments,
+            "isOwner": isOwner 
+        })
+    else:
+        return render(request, "auctions/listing.html", {
+            "listing": listingData,
+            "message": "Bid Failed!",
+            "update" : False,
+            "isListinginWatchinglist":isListinginWatchinglist,
+            "allComments":allComments,
+            "isOwner": isOwner 
+        })
+
+
+
+def displayWatchlist(request):
+    currentUser = request.user
+    listings = currentUser.listingWatchlist.all()
+    return render(request, "auctions/watchlist.html", {
+        "listings":listings
+    })
+
+
+def removeWatchlist(request, id):
+    listingData = Listing.objects.get(pk=id)
+    currentUser = request.user
+    listingData.watchlist.remove(currentUser)
+    return HttpResponseRedirect(reverse("listing", args=(id,)))
+
+
+def addWatchlist(request, id):
+    listingData = Listing.objects.get(pk=id)
+    currentUser = request.user
+    listingData.watchlist.add(currentUser)
+    return HttpResponseRedirect(reverse("listing", args=(id,)))
+
+
+def displayType(request):
+    if request.method == "POST":
+        typeFromForm = request.POST["type"]
+        type = Type.objects.get(typeName=typeFromForm)
+        activeListings = Listing.objects.filter(isActive=True, type=type)
+        allTypes = Type.objects.all()
+        return render(request, "auctions/index.html", {
+            "listings": activeListings,
+            "types": allTypes
+        })
+
+def createListing(request):
+    if request.method == "GET":
+        allTypes = Type.objects.all()
+        return render(request, "auctions/create.html", {
+            "types":allTypes
+        })
+    else:
+        name = request.POST["name"]
+        description = request.POST["description"]
+        imageurl = request.POST["imageurl"]
+        price = request.POST["price"]
+        type = request.POST["type"]
+
+        currentUser = request.user
+        typeData = Type.objects.get(typeName=type)
+
+        bid = Bid(bid=int(price), user=currentUser)
+        bid.save()
+
+        newListing = Listing(
+            name = name,
+            description = description,
+            imageUrl = imageurl,
+            price = bid,
+            type = typeData,
+            owner = currentUser
+        )
+        newListing.save()
+        return HttpResponseRedirect(reverse(index))
+
 
 
 def login_view(request):
